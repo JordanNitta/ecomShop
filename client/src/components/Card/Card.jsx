cef059b8b064c599b5c3bfcbb72e634c420e9d5b
JordanNitta <Jordannittarodrigues@gmail.com>
2023-06-16 20:36:09 -0700

    Fix link so it will direct to single item

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import axios from 'axios'
 const Card = () => {
     const [products, setProducts] = useState([])
-
+    const { id } = useParams
     useEffect(() => {
         const fetchProducts = async () => {
             const response = await axios.get('http://localhost:8000/api/all/products');
@@ -17,14 +18,14 @@ const Card = () => {
             {products.map((product) => {
                 const { _id, name, image, price, brand} = product;
                 return (
-                    <Link key={_id} className='pointer  bg-white'>
+                    <Link key={_id} to={`/products/${_id}`} className='pointer  bg-white'>
                         <div href="#" className='p-5'>
                             <img className="rounded-md" src={image} />
                         </div>
                         <div className="p-5">
                             <h5 className="mb-2 text-[14px] font-semibold text-gray-500">{brand}</h5>
                             <h5 className="mb-2 text-[14px] font-semibold text-black">{name}</h5>
-                            <h5 className="text-[14px] font-semibold text-black">${price}</h5>
+                            <h5 className="text-[14px] font-bold text-black">${price}</h5>
                         </div>
                     </Link>
                 );
