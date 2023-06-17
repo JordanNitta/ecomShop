77b89e38ceabfdf92e1514fd88fc250aec42688d
JordanNitta <Jordannittarodrigues@gmail.com>
2023-06-16 20:35:54 -0700

    Able to make a api call to receive the single item

--- client/src/pages/OneProduct.jsx
+++ client/src/pages/OneProduct.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
-
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import { useParams } from 'react-router-dom'
 const OneProduct = () => {
+    const { id } = useParams()
+
+    const [product, setProducts] = useState({})
+    useEffect(() => {
+        const fetchProducts = async () => {
+            const response = await axios.get(`http://localhost:8000/api/product/${id}`);
+            console.log(response.data)
+            setProducts(response.data);
+        };
+        fetchProducts()
+    }, [id]);
+
     return (
         <div>
-            
+            <h1> {product.name}</h1>
+            <h1> {product.brand}</h1>
+            <h1> {product.model}</h1>
+            <h1> {product.description}</h1>
+            <h1> {product.size}</h1>
+            <h1> {product.price}</h1>
+            <h1> {product.color}</h1>
         </div>
     )
 }
