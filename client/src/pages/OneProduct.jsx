8608ebba7df0080df08821265974aae39dc79323
JordanNitta <Jordannittarodrigues@gmail.com>
2023-06-17 22:47:03 -0700

    Working on small devices Tablet size responsive

--- client/src/pages/OneProduct.jsx
+++ client/src/pages/OneProduct.jsx
@@ -23,13 +23,12 @@ const OneProduct = ({selectedSize, handleBorder}) => {
     }, [id]);
 
     return (
-        <div className='bg-white md:p-0 lg:px-[150px] xl:px-[200px]'>
-            <div className='flex flex-col justify-center items-center sm:justify-center lg:flex-row lg:justify-between lg:items-center'>
-                <div className=''>
+        <div className='bg-white lg:px-[150px] xl:px-[200px]'>
+            <div className='px-5 md:px-10'>
                     <h1 className='text-[16px] font-500 text-gray-500 lg:hidden'>{product.brand}</h1>
                     <h1 className="text-[30px] font-semibold text-black lg:hidden">{product.name}</h1>
-                </div>
-                
+            </div>
+            <div className='flex flex-col justify-center items-center sm:justify-center lg:flex-row lg:justify-between lg:items-center'>
                 <img src={product.image} alt={product.name} width={500}/>
                 {/* Make sure to take off width full */}
                 <div className='lg:ml-[50px] space-y-5 w-full'>
@@ -38,7 +37,7 @@ const OneProduct = ({selectedSize, handleBorder}) => {
                     <h1 className='text-[16px] upper font-semibold'>Select a Size</h1>
                     <div className=''>
                         {product && product.size ? (
-                            <div className='hidden lg:grid lg:grid-cols-5 lg:gap-3 lg:mt-3 '>
+                            <div className='hidden lg:grid lg:grid-cols-5 lg:gap-[5px] lg:mt-3 '>
                                 {product.size.map((size, index) => (
                                     <button
                                         className={selectedSize === size ? 'font-style text-[14px] font-semibold border-[1px] border-black p-3' : 'font-style text-[14px] font-semibold border-[1px] border-gray-200 p-3'}
