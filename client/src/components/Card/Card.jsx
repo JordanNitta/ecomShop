c161688429a2910de07b85599467d173aa9f4962
JordanNitta <Jordannittarodrigues@gmail.com>
2023-06-19 21:48:48 -0700

    Working on responsive design for all products page

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,24 +1,21 @@
-import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
-import { useParams } from 'react-router-dom'
-import axios from 'axios'
-const Card = () => {
-    const [products, setProducts] = useState([])
-    const { id } = useParams
-    useEffect(() => {
-        const fetchProducts = async () => {
-            const response = await axios.get('http://localhost:8000/api/all/products');
-            setProducts(response.data);
-        };
-        fetchProducts()
-    }, []);
+const Card = ({products}) => {
+    // const [products, setProducts] = useState([])
+    // const { id } = useParams
+    // useEffect(() => {
+    //     const fetchProducts = async () => {
+    //         const response = await axios.get('http://localhost:8000/api/all/products');
+    //         setProducts(response.data);
+    //     };
+    //     fetchProducts()
+    // }, []);
 
     return (
-        <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
+        <div className='grid grid-cols-2 gap-2 md:grid-cols-3 md:gap-5 lg:grid-cols-3'>
             {products.map((product) => {
                 const { _id, name, image, price, brand} = product;
                 return (
-                    <Link key={_id} to={`/products/${_id}`} className='pointer  bg-white'>
+                    <Link key={_id} to={`/products/${_id}`} className='pointer  bg-white '>
                         <div href="#" className='p-5'>
                             <img className="rounded-md" src={image} />
                         </div>
