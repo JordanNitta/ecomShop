8849d9236665ad592e758b3a583c2f1603877fd5
JordanNitta <Jordannittarodrigues@gmail.com>
2023-07-16 12:20:29 -0700

    Got the apparel to display on home page

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -4,6 +4,8 @@ import { useParams, Link } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 const HomePage = () => {
     const [products, setProducts] = useState([])
+    const [apparel, setApparel] = useState([])
+
     const { id } = useParams
     useEffect(() => {
         const fetchProducts = async () => {
@@ -12,6 +14,14 @@ const HomePage = () => {
         };
         fetchProducts()
     }, []);
+
+    useEffect(() => {
+        const fetchApparel = async () => {
+            const response = await axios.get('http://localhost:8000/api/all/apparel');
+            setApparel(response.data)
+        }
+        fetchApparel()
+    }, [])
     return (
         <div className='h-screen w-full'>
             <div className="top-0 left-0 w-full h-screen z-10 flex justify-start items-center bg-no-repeat bg-cover sm:bg-center" style={{ backgroundImage: "url('https://cms-cdn.flightclub.com/3000/88afb62ea351-e62b-ee11-32c1-0c8e687d.jpg')" }}>
@@ -63,6 +73,28 @@ const HomePage = () => {
                                 </Link>
                             );
                         })}
+                    </div>
+                <div>
+                    <h1 className='font-bold text-mainColor text-[30px] mt-20'>Apparel</h1>
+                    <div className='grid grid-cols-2 gap-2 md:grid-cols-3  md:gap-5 lg:grid-cols-4 mt-8'>
+                        {apparel.map((apparel) => {
+                            const { _id, name, image, brand, model } = apparel;
+                            // if (model !== "550") {
+                            //     return null; 
+                            // }
+                            return (
+                                <Link key={_id} to={`/apparel/${_id}`} className='pointer bg-white'>
+                                    <div href="#" className='p-4'>
+                                        <img className="rounded-md" src={image} alt={name} />
+                                    </div>
+                                    <div className="p-5">
+                                        <h5 className="mb-2 text-[14px] font-semibold text-gray-500">{brand}</h5>
+                                        <h5 className="mb-2 text-[14px] font-semibold text-black">{name}</h5>
+                                    </div>
+                                </Link>
+                            );
+                        })}
+                    </div>
                     </div>
                 </div>
             </div>
