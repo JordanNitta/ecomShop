83785c023948d251b289dcbe07e0d0cfcaed202d
JordanNitta <Jordannittarodrigues@gmail.com>
2023-07-16 17:42:22 -0700

    Finished scroll color change on navbar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import Searchbar from './Searchbar'
 import { Link } from 'react-router-dom'
 const Navbar = () => {
     const [showMenu, setShowMenu] = useState(false)
-    const [hover, setHover] = useState(false)
+    const [scrollBg, setScrollBg] = useState(false)
     const navLinks = [
         { title: 'Sneakers', URL: '/sneakers' },
         { title: 'FC Apparel', URL: '/apparel' },
@@ -18,26 +18,37 @@ const Navbar = () => {
     }
 
     useEffect(() => {
-
-    })
+        const handleScroll = () => {
+            const scroll = window.scrollY > 800
+            setScrollBg(scroll)
+        }
+        window.addEventListener('scroll', handleScroll)
+        return () => {
+            window.removeEventListener('scroll', handleScroll)
+        }
+    }, [])
 
     return (
         <>
-            <div className='h-[76px] flex justify-between px-4 fixed z-50'>
-                <div className='flex items-center justify-between '>
+            <div className={`h-[76px] flex justify-between px-4 fixed z-50 w-full ${scrollBg ? 'bg-white' : 'bg-transparent'}`}>
+                <div className='flex items-center w-[45%]'>
                     <Searchbar />
-                    <Link to='/'>
-                        <img src={Logo} alt="" className='cursor-pointer w-[100px] lg:w-[160px]' />
-                    </Link>
                 </div>
-                <div className='hidden lg:flex lg:justify-end lg:items-center'>
+                <div className='hidden lg:flex lg:justify-between lg:items-center w-[60%]'>
+                    <div>
+                        <Link to='/'>
+                            <img src={Logo} alt="" className='cursor-pointer w-[100px] lg:w-[160px]' />
+                        </Link>
+                    </div>
+                    <div className='flex'>
                     {navLinks.map((link, index) => (
                         <ul key={index} className='lg:mx-3'>
                             <li>
-                                <Link to={link.URL}>{link.title}</Link>
+                                <Link to={link.URL} className={`text-[16px] font-medium ${scrollBg ? 'text-black' : 'text-white'}`}>{link.title}</Link>
                             </li>
                         </ul>
                     ))}
+                    </div>
                 </div>
                 <div onClick={handleMenu} className='lg:hidden my-auto'>
                     {showMenu ? <AiOutlineMenu size={30} className='cursor-pointer text-main ml-1 lg:hidden' /> : <AiOutlineClose size={30} className='cursor-pointer text-main ml-1 lg:hidden' />}
--- client/src/components/Navbar/Searchbar.jsx
+++ client/src/components/Navbar/Searchbar.jsx
@@ -3,14 +3,16 @@ import {AiOutlineSearch} from 'react-icons/ai'
 import { motion } from 'framer-motion'
 const Searchbar = () => {
     const [openSearch, setOpenSearch] = useState(false)
+    const [autoSearch, setAutoSearch] = useState('')
 
+    console.log(autoSearch)
     const handleSearch = () => {
         setOpenSearch(!openSearch)
     }
     return (
         <div className='flex items-center'>
             <span className='hidden lg:block relative left-7'><AiOutlineSearch size={20} color='grey'/></span>
-            <input type="text" className='hidden lg:block lg:text-[16px] sm:py-0 lg:px-30 lg:pl-10 lg:pr-10 lg:border-2 lg:leading-8 focus:outline-none' placeholder='Search' onClick={handleSearch}/>
+            <input type="text" className='hidden lg:block lg:text-[16px] sm:py-0 lg:px-30 lg:pl-10 lg:pr-10 lg:border-2 lg:leading-8 focus:outline-none' placeholder='Search' onClick={handleSearch} onChange={(e) => setAutoSearch(e.target.value)}/>
             
         </div>
     )
