1827f3c2adec12bb8cd7a22d0d5abb1c091b8d8c
JordanNitta <Jordannittarodrigues@gmail.com>
2023-07-17 22:21:18 -0700

    Need to finish styling the search bad component

--- client/src/components/Navbar/Searchbar.jsx
+++ client/src/components/Navbar/Searchbar.jsx
@@ -1,20 +1,23 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import axios from 'axios'
 import { AiOutlineSearch } from 'react-icons/ai'
-import { motion } from 'framer-motion'
+import { AnimatePresence, motion } from 'framer-motion'
 import { useNavigate } from 'react-router-dom'
 const Searchbar = () => {
     const navigate = useNavigate()
     const [openSearch, setOpenSearch] = useState(false)
     const [autoSearch, setAutoSearch] = useState('')
     const [products, setProducts] = useState([])
+    const closeSearchRef = useRef()
 
     console.log(autoSearch)
 
+    // Handle search bar when a user clicks it
     const handleOpenSearch = () => {
         setOpenSearch(!openSearch)
     }
 
+    // Makes an api call to get the products 
     useEffect(() => {
         const fetchProducts = async () => {
             const response = await axios.get('http://localhost:8000/api/all/products');
@@ -24,6 +27,7 @@ const Searchbar = () => {
     }, []);
     console.log(products)
 
+    // Handle when a user searches and hits enter
     const handleSearch = (e) => {
         const filterBySearch = products.filter((product) => autoSearch.toLowerCase() === "" ? product : product.name.toLowerCase().includes(autoSearch))
         if (e.key === 'Enter') {
@@ -34,18 +38,38 @@ const Searchbar = () => {
         }
     }
 
+    useEffect(() => {
+        const handleOutsideClick = (e) => {
+            if(closeSearchRef.current && !closeSearchRef.current.contains(e.target)) {
+                setOpenSearch(false)
+            }
+        }
+        document.addEventListener('mousedown', handleOutsideClick)
+
+        return (() => {
+            document.removeEventListener('mousedown', handleOutsideClick)
+        })
+    }, [])
+
     return (
         <>
-            <div className={`flex items-center z-20 ${openSearch ? 'w-full bg-white absolute h-full border-b-[3px]' : ''} `}>
+            <div 
+                ref={closeSearchRef}
+                className={`flex items-center z-20 ${openSearch ? 'w-full bg-white absolute h-full border-b-[3px]' : ''} `}>
                 <div className='flex justify-between items-center w-full'>
                     <div className='flex items-center'>
                         <span className='hidden lg:block relative left-7'><AiOutlineSearch size={20} color='grey' /></span>
-                        <input type="text" onClick={handleOpenSearch} className='hidden lg:block lg:text-[16px] sm:py-0 lg:px-30 lg:pl-10 lg:pr-10 lg:border-2 lg:leading-8 focus:outline-none' placeholder='Search' onKeyDown={handleSearch} onChange={(e) => setAutoSearch(e.target.value)} />
+                        <input type="text" onClick={handleOpenSearch} 
+                        className='hidden lg:block lg:text-[16px] sm:py-0 lg:px-30 lg:pl-10 lg:pr-10 lg:border-2 lg:leading-8 focus:outline-none'
+                        placeholder='Search' 
+                        style={{ border: openSearch ? 'none' : '' }} 
+                        onKeyDown={handleSearch} 
+                        onChange={(e) => setAutoSearch(e.target.value)} />
                     </div>
                     <div className='flex'>
                         {openSearch ? (
                             <div className=''>
-                                <button className='h-[76px] w-[90px]' style={{ borderLeft: "3px solid rgb(234, 234, 234)"}}>Close</button>
+                                <button onClick={handleOpenSearch} className='h-[76px] w-[90px]' style={{ borderLeft: "3px solid rgb(234, 234, 234)"}}>Close</button>
                             </div>
                         ) : (
                             <div>
@@ -55,24 +79,28 @@ const Searchbar = () => {
                 </div>
             </div>
             {openSearch && ( // Show the bottom bar only when the search bar is open
+            <AnimatePresence>
                 <motion.div 
-                    initial={{ y: -400, opacity: 0 }}
+                    initial={{ y: -500, opacity: 0 }}
                     animate={{ y: 0, opacity: 1 }}
-                    exit={{ y: 0, opacity: -0 }}
-                    transition={{ duration: .8 }}
+                    exit={{ y: 0, opacity: 1 }}
+                    transition={{ duration: .6}}
                     className='w-full h-[400px] absolute top-[4rem] bg-white  border-[rgb(234,234,234)] z-10'>
-                    {autoSearch &&
+                    {openSearch &&
                         products
                             .filter((product) => autoSearch.toLowerCase() === '' ? product : product.name.toLowerCase().includes(autoSearch))
                             .map((product) => (
                                 <ul key={product._id}>
-                                    <li>{product.name}</li>
+                                    <div className='flex items-center'>
+                                        <img src={product.image} className='w-[100px]' />
+                                        <li className='ml-5'>{product.name}</li>
+                                    </div>
                                     <li>{product.model}</li>
                                     <li>{product.brand}</li>
-                                    <img src={product.image} className='w-[100px]' />
                                 </ul>
                             ))}
                 </motion.div>
+            </AnimatePresence>
             )}
         </>
     )
