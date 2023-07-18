1276d2f27ec731323ff2c4b3900c131151669275
JordanNitta <Jordannittarodrigues@gmail.com>
2023-07-18 15:49:49 -0700

    Working on responsive design on the nav bar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,22 +1,23 @@
-import { useContext, useEffect, useState } from 'react'
-import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'
-import Logo from '../../assets/Img/Logo.png'
-import Searchbar from './Searchbar'
+import { useContext, useEffect, useState, useRef } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import UserContext from '../../context/UserContext'
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'
+import { AnimatePresence, motion } from 'framer-motion'
+import Logo from '../../assets/Img/Logo.png'
+import Searchbar from './Searchbar'
 const Navbar = () => {
     const navigate = useNavigate()
     const [showMenu, setShowMenu] = useState(false)
     const [scrollBg, setScrollBg] = useState(false)
-
+    const navBarRef = useRef()
     const { loggedUser, setLoggedUser } = useContext(UserContext)
-    const navLinks = [
-        { title: 'Sneakers', URL: '/sneakers' },
-        { title: 'FC Apparel', URL: '/apparel' },
-        { title: 'Stores', URL: '/stores' },
-        { title: 'Account', URL: '/register' },
-    ]
+    // const navLinks = [
+    //     { title: 'Sneakers', URL: '/sneakers' },
+    //     { title: 'FC Apparel', URL: '/apparel' },
+    //     { title: 'Stores', URL: '/stores' },
+    //     { title: 'Account', URL: '/register' },
+    // ]
 
     const handleMenu = () => {
         setShowMenu(!showMenu)
@@ -31,31 +32,41 @@ const Navbar = () => {
             .catch((err) => {
                 console.log(err, "Can't log out")
             })
-    } 
+    }
 
     useEffect(() => {
         axios.get("http://localhost:8000/api/logged/user", { withCredentials: true })
-        .then((res) => {
-            setLoggedUser(res.data)
-        })
-        .catch(() => {
+            .then((res) => {
+                setLoggedUser(res.data)
+            })
+            .catch(() => {
 
-        })
+            })
+
         const handleScroll = () => {
-            const scroll = window.scrollY > 800
-            setScrollBg(scroll)
+            if (window.scrollY > 100) {
+                navBarRef.current.style.backgroundColor = 'white'
+                navBarRef.current.style.color = 'black'
+            }
+            else {
+                navBarRef.current.style.backgroundColor = 'transparent'
+                navBarRef.current.style.color = 'white'
+            }
         }
         window.addEventListener('scroll', handleScroll)
         return () => {
             window.removeEventListener('scroll', handleScroll)
         }
+
     }, [])
 
     return (
         <>
-            <div className={`h-[76px] flex justify-between fixed z-50 w-full ${scrollBg ? 'bg-white' : 'bg-transparent'}`}>
+            <div
+                ref={navBarRef}
+                className='h-[76px] flex justify-between fixed z-50 w-full'>
                 <div className='flex items-center w-[45%]'>
-                    <Searchbar/>
+                    <Searchbar />
                 </div>
                 <div className='hidden lg:flex lg:justify-between lg:items-center w-[60%]'>
                     <div>
@@ -64,21 +75,64 @@ const Navbar = () => {
                         </Link>
                     </div>
                     <div className='flex'>
-                    {navLinks.map((link, index) => (
-                        <ul key={index} className='lg:mx-3'>
+                        {/* {navLinks.map((link, index) => ( */}
+                        <ul className='lg:mx-3 flex space-x-5'>
                             <li>
-                                <Link to={link.URL} className={`text-[16px] font-medium ${scrollBg ? 'text-black' : 'text-white'}`}>{link.title}</Link>
+                                <Link to='/sneakers' className='text-[16px] font-medium'>Sneakers</Link>
+                            </li>
+                            <li>
+                                <Link to='/apparel' className='text-[16px] font-medium'>FC Apparel</Link>
+                            </li>
+                            <li>
+                                <Link to='/store' className='text-[16px] font-medium'>Store</Link>
+                            </li>
+                            <li>
+                                <Link to='/account' className='text-[16px] font-medium'>Login</Link>
+                            </li>
+                            <li>
+                                <Link to='/account' className='text-[16px] font-medium'>Sign Up</Link>
                             </li>
                         </ul>
-                    ))}
+                        {/* // ))} */}
                     </div>
-                    <h6 className='text-black'>{loggedUser?.firstName}</h6>
+                    {/* <h6 className='text-black'>{loggedUser?.firstName}</h6> */}
                 </div>
-                <button onClick={handleLoggedOut}>LogOut</button>
+                {/* <button onClick={handleLoggedOut}>LogOut</button> */}
 
-                <div onClick={handleMenu} className='lg:hidden my-auto'>
-                    {showMenu ? <AiOutlineMenu size={30} className='cursor-pointer text-main ml-1 lg:hidden' /> : <AiOutlineClose size={30} className='cursor-pointer text-main ml-1 lg:hidden' />}
-                </div>
+                <motion.div 
+                    onClick={handleMenu} 
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    className={`flex justify-end right-0 absolute h-[76px] lg:hidden ${showMenu ? 'bg-white border-b-[3px] w-full z-30   border-[rgb(234,234,234)]' : ''}`}>
+                    <div className='flex items-center '>
+                        {showMenu ? (
+                            <AiOutlineClose
+                                size={30}
+                                className='cursor-pointer text-main ml-1 text-black lg:hidden'
+                            />
+                            
+                        ) : (
+                            <AiOutlineMenu
+                                size={30}
+                                className='cursor-pointer text-main ml-1 text-black lg:hidden'
+                            />
+
+                        )}                   
+                    </div>
+                </motion.div>
+                {showMenu && (
+                    <motion.div
+                        initial={{ y: -1000, opacity: 0 }}
+                        animate={{ y: 0, opacity: 1 }}
+                        exit={{ y: 0, opacity: 1 }}
+                        transition={{ duration: .3 }}
+                        className='fixed top-[76px] w-full h-full bg-white z-20'>
+                        {/* Add your menu items here */}
+                        <p className='text-black'>JI</p>
+                        <p>2</p>
+                        <p>3</p>
+                    </motion.div>
+                )}
             </div>
         </>
         // <>
--- client/src/components/Navbar/Searchbar.jsx
+++ client/src/components/Navbar/Searchbar.jsx
@@ -3,7 +3,7 @@ import axios from 'axios'
 import { AiOutlineSearch } from 'react-icons/ai'
 import { AnimatePresence, motion } from 'framer-motion'
 import { useNavigate } from 'react-router-dom'
-const Searchbar = () => {
+const Searchbar = ( { handleScroll }) => {
     const navigate = useNavigate()
     const [openSearch, setOpenSearch] = useState(false)
     const [autoSearch, setAutoSearch] = useState('')
@@ -58,9 +58,9 @@ const Searchbar = () => {
                 className={`flex items-center z-20 ${openSearch ? 'w-full bg-white absolute h-full border-b-[3px]' : ''} `}>
                 <div className='flex justify-between items-center w-full'>
                     <div className='flex items-center'>
-                        <span className='hidden lg:block relative left-7'><AiOutlineSearch size={20} color='grey' /></span>
+                        <span className='hidden md:block md:relative md:left-12'><AiOutlineSearch size={20} color='grey' /></span>
                         <input type="text" onClick={handleOpenSearch} 
-                        className='hidden lg:block lg:text-[16px] sm:py-0 lg:px-30 lg:pl-10 lg:pr-10 lg:border-2 lg:leading-8 focus:outline-none'
+                        className='bg-transparent ml-5 md:block md:text-[16px] sm:py-0 lg:px-30 md:pl-10 md:pr-10 md:border-2 md:leading-8 focus:outline-none '
                         placeholder='Search' 
                         style={{ border: openSearch ? 'none' : '' }} 
                         onKeyDown={handleSearch} 
@@ -69,7 +69,7 @@ const Searchbar = () => {
                     <div className='flex'>
                         {openSearch ? (
                             <div className=''>
-                                <button onClick={handleOpenSearch} className='h-[76px] w-[90px]' style={{ borderLeft: "3px solid rgb(234, 234, 234)"}}>Close</button>
+                                <button onClick={handleOpenSearch} className='h-[76px] w-[90px] text-black' style={{ borderLeft: "3px solid rgb(234, 234, 234)"}}>Close</button>
                             </div>
                         ) : (
                             <div>
@@ -85,20 +85,21 @@ const Searchbar = () => {
                     animate={{ y: 0, opacity: 1 }}
                     exit={{ y: 0, opacity: 1 }}
                     transition={{ duration: .6}}
-                    className='w-full h-[400px] absolute top-[4rem] bg-white  border-[rgb(234,234,234)] z-10'>
-                    {openSearch &&
-                        products
-                            .filter((product) => autoSearch.toLowerCase() === '' ? product : product.name.toLowerCase().includes(autoSearch))
-                            .map((product) => (
-                                <ul key={product._id}>
-                                    <div className='flex items-center'>
-                                        <img src={product.image} className='w-[100px]' />
-                                        <li className='ml-5'>{product.name}</li>
+                    className='w-full h-screen space-y-1 overflow-y-auto md:h-screen lg:h-[600px] lg:flex lg:flex-col lg:flex-wrap absolute top-[4rem] bg-white  border-[rgb(234,234,234)] z-10'>
+                        {openSearch &&
+                            products
+                                .filter((product) => autoSearch.toLowerCase() === '' ? product : product.name.toLowerCase().includes(autoSearch))
+                                .slice(0, 8)
+                                .map((product) => (
+                                    <div className='px-5 h-[80px]'>
+
+                                            <div className='flex items-center h-[80px] space-y-7 '>
+                                                <img src={product.image} className='w-[100px]' />
+                                                <h6 className='ml-12 text-black'>{product.name}</h6>
+                                            </div>
                                     </div>
-                                    <li>{product.model}</li>
-                                    <li>{product.brand}</li>
-                                </ul>
-                            ))}
+                            
+                                ))}
                 </motion.div>
             </AnimatePresence>
             )}
